5c172d458996b1e657c706c7a6ed6424a58f8e4b
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-08-09 23:52:08 +0100

    Update answer.js

--- answer.js
+++ answer.js
@@ -12,10 +12,20 @@ const functionName = (n,m) => {
   }
   return totalOfSum
 }
-
+/*2nd Solution  */
 function sumMul(n,m){
   if (n <= 0 || m <= 0) return "INVALID";
   
   const last = Math.ceil(m/n) * n - n;
   return (last + n) * (last / n) / 2;
+}
+
+/*3rd Solution  */
+function sumMul(n,m){
+  if (n >= m) return "INVALID";
+var sum = 0;
+  for (var i = n; i < m; i+=n) {
+    sum += i;
+  }
+  return sum;
 }
